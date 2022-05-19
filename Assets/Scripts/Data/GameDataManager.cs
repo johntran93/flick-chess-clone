6c7922b83580c7168b6ddaaa40826d086a79aeae
NguyenHung <Nguyenhung104307@gmail.com>
2022-05-19 11:32:54 +0700

    - fix coding convention

--- Assets/Scripts/Data/GameDataManager.cs
+++ Assets/Scripts/Data/GameDataManager.cs
@@ -6,11 +6,11 @@ using System.IO;
 public class GameDataManager : MonoBehaviour
 {
     public static GameDataManager Instance;
-    private Data data;
-    private string paths = "";
-    private string persistanPaths = "";
+    private Data _data;
+    private string _paths = "";
+    private string _persistanPaths = "";
 
-    public Data Data { get { return data; } set { data = value; } }
+    public Data Data { get { return _data; } set { _data = value; } }
     private void Awake()
     {
         if (Instance == null)
@@ -29,20 +29,20 @@ public class GameDataManager : MonoBehaviour
     }
     private void CreatPlayerData()
     {
-        data = new Data();
+        _data = new Data();
     }
     private void SetPaths()
     {
-        paths = Application.dataPath + Path.AltDirectorySeparatorChar + "SaveData.json";
-        persistanPaths = Application.persistentDataPath + Path.AltDirectorySeparatorChar + "SaveData.json";
+        _paths = Application.dataPath + Path.AltDirectorySeparatorChar + "SaveData.json";
+        _persistanPaths = Application.persistentDataPath + Path.AltDirectorySeparatorChar + "SaveData.json";
     }
 
     public void SaveData()
     {
-        string savePath = persistanPaths;
+        string savePath = _persistanPaths;
 
         Debug.Log("Saving Data");
-        string json = JsonUtility.ToJson(data);
+        string json = JsonUtility.ToJson(_data);
         Debug.Log(json);
 
         StreamWriter writer = new StreamWriter(savePath);
@@ -50,10 +50,10 @@ public class GameDataManager : MonoBehaviour
     }
     public void LoadData()
     {
-        using StreamReader reader = new StreamReader(persistanPaths);
+        using StreamReader reader = new StreamReader(_persistanPaths);
         string json = reader.ReadToEnd();
 
-        data = JsonUtility.FromJson<Data>(json);
-        Debug.Log(data.Level);
+        _data = JsonUtility.FromJson<Data>(json);
+        Debug.Log(_data.Level);
     }
 }
